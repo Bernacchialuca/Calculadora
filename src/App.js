e3d2dbf58a20f3540ceb7827c9b0e5e73fce1534
Luca <lucabernacchia98@gmail.com>
2023-01-11 15:06:00 -0300

    fix

--- src/App.js
+++ src/App.js
@@ -41,13 +41,19 @@ const App = () => {
           <Boton manejarClick={agregarInput} >7</Boton>
           <Boton manejarClick={agregarInput} >8</Boton>
           <Boton manejarClick={agregarInput} >9</Boton>
-          <Boton manejarClick={agregarInput} >-</Boton>
+          <Boton manejarClick={agregarInput} >^</Boton>
         </div>
         <div className='fila'>
-          <Boton manejarClick={agregarInput} >6</Boton>
-          <Boton manejarClick={agregarInput} >5</Boton>
           <Boton manejarClick={agregarInput} >4</Boton>
+          <Boton manejarClick={agregarInput} >5</Boton>
+          <Boton manejarClick={agregarInput} >6</Boton>
           <Boton manejarClick={agregarInput} >+</Boton>
+        </div>
+        <div className='fila'>
+          <Boton manejarClick={agregarInput} >1</Boton>
+          <Boton manejarClick={agregarInput} >2</Boton>
+          <Boton manejarClick={agregarInput} >3</Boton>
+          <Boton manejarClick={agregarInput} >-</Boton>
         </div>
         <div className='fila'>
           <Boton manejarClick={agregarInput} >.</Boton>
